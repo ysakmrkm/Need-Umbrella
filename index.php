8c6972e79e82ab44310d29978aea1e53cc1aa171
Yoshiaki Murakami <info@ysakmrkm.com>
2012-02-12 03:10:19 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>傘いる？</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://www.ysakmrkm.com/umbrella/js/function.js"></script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-15639132-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</head>
+<body>
+<article>
+<h1>傘いる？</h1>
+
+<form name="inputform" action="" method="POST">
+<select id="pref" name="pref">
+<option value="" selected="selected">都道府県</option>
+<option value="01">北海道</option>
+<option value="02">青森県</option>
+<option value="03">岩手県</option>
+<option value="04">宮城県</option>
+<option value="05">秋田県</option>
+<option value="06">山形県</option>
+<option value="07">福島県</option>
+<option value="08">茨城県</option>
+<option value="09">栃木県</option>
+<option value="10">群馬県</option>
+<option value="11">埼玉県</option>
+<option value="12">千葉県</option>
+<option value="13">東京都</option>
+<option value="14">神奈川県</option>
+<option value="15">新潟県</option>
+<option value="16">富山県</option>
+<option value="17">石川県</option>
+<option value="18">福井県</option>
+<option value="19">山梨県</option>
+<option value="20">長野県</option>
+<option value="21">岐阜県</option>
+<option value="22">静岡県</option>
+<option value="23">愛知県</option>
+<option value="24">三重県</option>
+<option value="25">滋賀県</option>
+<option value="26">京都府</option>
+<option value="27">大阪府</option>
+<option value="28">兵庫県</option>
+<option value="29">奈良県</option>
+<option value="30">和歌山県</option>
+<option value="31">鳥取県</option>
+<option value="32">島根県</option>
+<option value="33">岡山県</option>
+<option value="34">広島県</option>
+<option value="35">山口県</option>
+<option value="36">徳島県</option>
+<option value="37">香川県</option>
+<option value="38">愛媛県</option>
+<option value="39">高知県</option>
+<option value="40">福岡県</option>
+<option value="41">佐賀県</option>
+<option value="42">長崎県</option>
+<option value="43">熊本県</option>
+<option value="44">大分県</option>
+<option value="45">宮崎県</option>
+<option value="46">鹿児島県</option>
+<option value="47">沖縄県</option>
+</select>
+
+<select id="area" name="area">
+<option value="" selected="selected">エリア</option>
+</select>
+
+<input type="submit" value="都道府県を選択してね" disabled="disabled" />
+
+</form>
+
+<div id="answer"></div>
+</article>
+</body>
+</html>
---
+++ js/function.js
@@ -0,0 +1,144 @@
+$(function(){
+	//localStorageに都道府県情報有り
+	if(localStorage.pref !== undefined){
+		selectedPref = localStorage.pref;
+
+		$('#pref').children('option').each(function(){
+			if($(this).attr('value') == selectedPref){
+				$(this).attr('selected','selected');
+			}
+		});
+
+		$json = 'http://www.drk7.jp/weather/json/'+selectedPref+'.js';
+		$.getJSON($json+'?callback=?');
+	} else {
+		selectedPref = '';
+	}
+
+	//localStorageにエリア情報有り
+	if(localStorage.area !== undefined){
+		selectedArea = localStorage.area;
+	} else {
+		selectedArea = '';
+	}
+
+	//localStorageに都道府県・エリア情報有り
+	if(localStorage.pref !== undefined && localStorage.area !== undefined){
+		$('input[type=submit]').attr('value','再選択');
+		$('input[type=submit]').removeAttr('class');
+		$('input[type=submit]').removeAttr('disabled');
+	}
+
+	$('select').change(function(){
+		//エリア選択時
+		if($(this).attr('id').indexOf('pref') !== -1){
+			$('#area').children('option').not(':first-child').remove();
+		}
+
+		selectedPref = $('#pref').children(':selected').attr('value');
+		selectedArea = $('#area').children(':selected').attr('value');
+
+		if(selectedPref === ''){
+			$(this).siblings('input[type=submit]').attr('value','都道府県を選択してね');
+			$(this).siblings('input[type=submit]').attr('disabled','disabled');
+		}
+
+		if(selectedPref !== '' && selectedArea === ''){
+			$(this).siblings('input[type=submit]').attr('value','エリアを選択してね');
+			$(this).siblings('input[type=submit]').attr('disabled','disabled');
+
+			$json = 'http://www.drk7.jp/weather/json/'+selectedPref+'.js';
+			$.getJSON($json+'?callback=?');
+		}
+
+		if(selectedArea !== ''){
+			$(this).siblings('input[type=submit]').attr('value','確認する');
+			$(this).siblings('input[type=submit]').removeAttr('class');
+			$(this).siblings('input[type=submit]').addClass('check');
+			$(this).siblings('input[type=submit]').removeAttr('disabled');
+		}
+	});
+
+	$('input[type=submit]').click(function(){
+		$('#answer').children().remove();
+		if($(this).hasClass('check')){
+			localStorage.pref = selectedPref;
+			localStorage.area = selectedArea;
+			$json = 'http://www.drk7.jp/weather/json/'+selectedPref+'.js';
+			$.getJSON($json+'?callback=?');
+		} else {
+			localStorage.clear();
+			selectedPref = '';
+			selectedArea = '';
+			$('select').each(function(){
+				$(this).removeAttr('selected');
+			});
+			$('select').children('option:first-child').attr('selected','selected');
+			$(this).attr('disabled','disabled');
+			$(this).attr('value','都道府県を選択してね');
+		}
+	});
+
+	$('form').submit(function(){
+		localStorage.pref = selectedPref;
+		localStorage.area = selectedArea;
+		if($('input[type=submit]').hasClass('check')){
+		$('input[type=submit]').attr('value','再選択');
+		$('input[type=submit]').removeAttr('class');
+		} else {
+		}
+		return false;
+	});
+
+});
+
+function drk7jpweather(){}
+
+drk7jpweather.callback = function(data){
+		var areas = data.pref.area;
+	if($('#area').children('option').length == 1){
+		var areaOption = new Array();
+
+		for(var i in areas){
+			areaOption.push(i);
+		}
+		areaOption.sort();
+
+		for(var j in areaOption){
+			$('<option value="'+areaOption[j]+'">'+areaOption[j]+'</option>').appendTo('#area');
+		}
+
+		$('#area').children('option').each(function(){
+			if($(this).attr('value') == selectedArea){
+			$(this).attr('selected','selected');
+			}
+		});
+	}
+
+	if(selectedPref !== '' && selectedArea !== ''){
+		for(var k in areas){
+			if(k === selectedArea){
+				rainfallChance = data.pref.area[k].info[0].rainfallchance.period;
+				
+		total = 0;
+		for(var i in rainfallChance){
+			total = total + parseInt(rainfallChance[i].content);
+		}
+		if(total == 0){
+			answer = '快晴！！';
+		} else if(total >= 1 && total <= 40){
+			answer = '必要無いかも。';
+		} else if(total >= 41 && total <= 160){
+			answer = 'あると助かるかな？';
+		} else if(total >= 161 && total <= 240){
+			answer = 'あって損はないよ。';
+		} else if(total >= 241 && total <= 320){
+			answer = '無いと厳しいかも。';
+		} else if(total >= 321 && total <= 400){
+			answer = '絶対いるよ！！';
+		}
+		$('#answer').append('<p>'+answer+'</p>');
+			}
+		}
+	}
+}
